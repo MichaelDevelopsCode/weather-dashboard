db5191c2316965e27da9b53b11a90a40006ac958
Michael Arvelo <michaeldevelopscode@gmail.com>
2020-11-21 17:25:12 -0500

    loads hard coded cities in history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 var apiKey = "appId=bc9f6ddfab3a7a8e6e4b64e7c936aeb9";
 var searchEl = document.getElementById("citySearch");
+var historyEl = document.getElementById("history"); 
 
-var savedCities = [];
+var savedCities = ["altamonte springs", "orlando", "new york"];
+
+// load history from local storage if available
+var loadHistory = function() {
+    // add all cities to list element
+    savedCities.forEach(element => {
+        // create list elements and add text
+        var cityItem = document.createElement("li");
+        cityItem.className = "list-group-item";
+        cityItem.textContent = element;
+
+        // append to ul element
+        historyEl.appendChild(cityItem);
+    });
+};
 
 
 var citySearchSubmit = function(event) {
@@ -28,6 +43,6 @@ var citySearchSubmit = function(event) {
 }
 
 
-
+loadHistory();
 // On submit call function to search for city
 document.addEventListener("submit", citySearchSubmit);
